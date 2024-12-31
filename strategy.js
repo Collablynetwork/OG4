f9350858679e272e30336d755f9e9c255ede63b9
Collably Network <165827418+Collablynetwork@users.noreply.github.com>
2024-12-31 09:20:23 +0530

    Update strategy.js

--- strategy.js
+++ strategy.js
@@ -1,6 +1,6 @@
 export function checkConditions(rsi1d, rsi4h, rsi15m, rsi1m, priceChangePercent) {
     return (
-        priceChangePercent >= -15 && priceChangePercent <= 50 && // Updated condition for priceChangePercent
+        priceChangePercent >= -100 && priceChangePercent <= 100 && // Updated condition for priceChangePercent
         rsi1d >= 50 && rsi1d <= 60 &&
         rsi4h >= 55 && rsi4h <= 65 &&
         rsi15m >= 20 && rsi15m <= 40 &&
